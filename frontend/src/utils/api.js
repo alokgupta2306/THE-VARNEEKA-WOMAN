ece08f6bf9a436beeee68d27170b297ca42733b4
Alok Gupta <alokguptatute23@gmail.com>
2026-03-28 00:23:52 +0530

    Add netlify config

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -4,7 +4,6 @@ const API = axios.create({
   baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api'
 });
 
-// Add token to every request automatically
 API.interceptors.request.use((req) => {
   const user = localStorage.getItem('varneekaUser');
   if (user) {
